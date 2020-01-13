6bbc476b2ed4396a545b16fc5af358713a7b217f
theJohnShow <57822778+theJohnShow@users.noreply.github.com>
2020-01-12 23:07:07 -0500

    Add source and multiple formats

--- home.js
+++ home.js
@@ -3,7 +3,7 @@
 var episodesToLoad = 5
 
 function iterateHTML(allCasts) {
-  return '\n        <div class="episode">\n          <div class="epTitle">'+allCasts[0]+'</div>\n          <div class="epDescription">\n            '+allCasts[1]+'\n          </div>\n          <audio controls preload="metadata" src="'+allCasts[2]+'"></audio>\n          <div class="epCast">'+allCasts[3]+'</div><div class="epDate">'+allCasts[4]+'</div>\n        </div>\n';
+  return '\n        <div class="episode">\n          <div class="epTitle">'+allCasts[0]+'</div>\n          <div class="epDescription">\n            '+allCasts[1]+'\n          </div>\n          <audio controls preload="metadata">\n            <source src="'+allCasts[2]+'.ogg" type="audio/ogg">\n            <source src="'+allCasts[2]+'.mp3" type="audio/mpeg">\n          </audio>\n          <div class="epCast">'+allCasts[3]+'</div><div class="epDate">'+allCasts[4]+'</div>\n        </div>\n';
 }
 
 var xmlRequest = new XMLHttpRequest();
